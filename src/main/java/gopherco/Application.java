c74017370fe66cfb29a5dcf334fe7734c8faacc4
MIZINCHIK <mizinchik@yahoo.com>
2023-11-15 21:15:14 +0700

    Application: Implement App entry

--- src/main/java/gopherco/Application.java
+++ src/main/java/gopherco/Application.java
@@ -1,12 +1,33 @@
-package gopherco.editor;
+package gopherco;
+
+import gopherco.io.console.ConsoleInput;
+import gopherco.io.console.UserChoiceProvider;
+import gopherco.menu.Menu;
+import gopherco.menu.MenuBuilder;
+import gopherco.menu.MenuSwitcher;
+import gopherco.menu.context.ApplicationContext;
+import gopherco.menu.render.MenuRenderer;
 
 public class Application {
-
     private Application() {
         throw new IllegalStateException();
     }
 
     public static void main(String[] args) {
-
+        Configuration configuration = Configuration.getInstance();
+        ApplicationContext applicationContext = ApplicationContext.getInstance();
+        ConsoleInput input = new ConsoleInput();
+        MenuBuilder menuBuilder = new MenuBuilder(applicationContext, input, configuration);
+        Menu mainMenu = menuBuilder.build();
+        MenuRenderer renderer = new MenuRenderer();
+        MenuSwitcher menuSwitcher = new MenuSwitcher(mainMenu, renderer, new UserChoiceProvider(input));
+        while (applicationContext.isRunning()) {
+            try {
+                menuSwitcher.doSwitch();
+            } catch (Exception e) {
+                e.printStackTrace();
+                ApplicationContext.getInstance().shutdown();
+            }
+        }
     }
 }
