38d47d74fe3f198a7eec1a64d93b490211efd888
MIZINCHIK <mizinchik@yahoo.com>
2023-11-15 15:11:06 +0700

    Menu Items: Create interfaces for items and handlers

---
+++ src/main/java/gopherco/menu/item/Item.java
@@ -0,0 +1,8 @@
+package gopherco.menu.item;
+
+public interface Item {
+    int getPosition();
+    String getTitle();
+    ItemHandler getItemHandler();
+    Item copyWithPosition(int position);
+}
