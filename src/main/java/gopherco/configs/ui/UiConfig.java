1f1f0197a47b4b949eeab0e5a65e946add5c80b6
MIZINCHIK <mizinchik@yahoo.com>
2023-11-29 19:35:04 +0700

    UI Config: Create config classes

---
+++ src/main/java/gopherco/configs/ui/UiConfig.java
@@ -0,0 +1,7 @@
+package gopherco.configs.ui;
+
+import java.awt.Color;
+
+public record UiConfig(Color menuButtonPlayColor, Color menuButtonReplaysColor, Color menuButtonExitColor,
+                       Color menuMainColor, Color menuBackgroundColor, Color menuLeftSidebarImage) {
+}
